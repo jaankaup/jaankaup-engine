c5e95ec56245f848c64500a2ff3f3ce023fcda22
Janne Kauppinen <janne.a.kauppinen@student.jyu.fi>
2021-12-25 15:29:01 +0200

    The core dependencies.

---
+++ core/src/lib.rs
@@ -0,0 +1,15 @@
+// pub mod wgpu_system;
+// pub mod input;
+// pub mod shader;
+// pub mod misc;
+// pub mod buffer;
+// pub mod texture;
+// pub mod assets;
+// pub mod camera;
+// pub mod two_triangles;
+// pub mod mc;
+// pub mod temp;
+// pub mod render_pipelines;
+// pub mod noise3d;
+// pub mod compute;
+pub use wgpu;
